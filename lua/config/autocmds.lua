fcca9c43af3281cc8b059a2b744de78c92dee699
mich-murphy <github@elmurphy.com>
2024-02-18 21:37:32 +1100

    refactoring autocmds and adding autogrp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,8 +2,14 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- disable lsp_lines in lazy.vim
+-- create autogroup to trigger autocmds only once
+local function augroup(name)
+  return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
+end
+
+-- disable lsp_lines in lazy.vim floating window
 vim.api.nvim_create_autocmd("WinEnter", {
+  group = augroup("disable_lsp_lines"),
   callback = function()
     local floating = vim.api.nvim_win_get_config(0).relative ~= ""
     vim.diagnostic.config({
@@ -13,21 +19,26 @@ vim.api.nvim_create_autocmd("WinEnter", {
   end,
 })
 
+-- set Zellij mode
+local function zellij(mode)
+  if vim.env.ZELLIJ ~= nil then
+    vim.fn.system({ "zellij", "action", "switch-mode", mode })
+  end
+end
+
 -- set Zellij to lock with Neovim is open: refer to help autocmd-events
 vim.api.nvim_create_autocmd("BufEnter", {
+  group = augroup("zellij_lock"),
   callback = function()
-    if vim.env.ZELLIJ ~= nil then
-      vim.fn.system({ "zellij", "action", "switch-mode", "locked" })
-    end
-  end,
+    zellij("locked")
+  end
 })
 
 -- set Zellij to normal when Neovim exits
 vim.api.nvim_create_autocmd("VimLeave", {
+  group = augroup("zellij_normal"),
   callback = function()
-    if vim.env.ZELLIJ ~= nil then
-      vim.fn.system({ "zellij", "action", "switch-mode", "normal" })
-    end
-  end,
+    zellij("normal")
+  end
 })
 
