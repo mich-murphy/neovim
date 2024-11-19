8a127fe77be74628260805f871417c22a0288d7d
mich-murphy <github@elmurphy.com>
2024-11-19 21:58:16 +1100

    feat: removing nvim-dashboard config as lazyvim no longer uses it

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -20,24 +20,4 @@ return {
       })
     end,
   },
-
-  -- change dashboard logo
-  {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      local logo = [[
-███╗   ██╗ ███████╗  ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗
-████╗  ██║ ██╔════╝ ██╔═══██╗ ██║   ██║ ██║ ████╗ ████║
-██╔██╗ ██║ █████╗   ██║   ██║ ██║   ██║ ██║ ██╔████╔██║
-██║╚██╗██║ ██╔══╝   ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║
-██║ ╚████║ ███████╗ ╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║
-╚═╝  ╚═══╝ ╚══════╝  ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝
-
-          
-      Talk is cheap. Show me the code. 
-      ]]
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
-    end,
-  },
 }
